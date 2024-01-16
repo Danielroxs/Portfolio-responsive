b378069a8e0f5d949171bf05b2b381813dc72652
Danielroxs <76076561+Danielroxs@users.noreply.github.com>
2024-01-16 17:00:41 -0600

    Contact form & encrypt email finished

--- script.js
+++ script.js
@@ -28,11 +28,11 @@ window.onscroll = () => {
 }
 /* sticky navbar */
 window.onscroll = () => {
-let header = document.querySelector('.header');
+    let header = document.querySelector('.header');
 
-header.classList.toggle('sticky', window.scrollY > 100)
+    header.classList.toggle('sticky', window.scrollY > 100)
 
-/* remove menu icon navbar when click navbar link (scroll) */
+    /* remove menu icon navbar when click navbar link (scroll) */
 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
@@ -84,10 +84,10 @@ darkModeIcon.onclick = () => {
 /* scroll reveal */
 
 ScrollReveal({
-     /* reset: true, */
-     distance: '80px',
-     duration: 2000,
-     delay: 200 
+    /* reset: true, */
+    distance: '80px',
+    duration: 2000,
+    delay: 200
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
@@ -106,32 +106,87 @@ const subject = document.getElementById("subject");
 const mess = document.getElementById("message");
 
 function sendEmail() {
-    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
 
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "danroxsll@gmail.com",
-        Password : "2EDCBA94950051F8A02F7B5A7D129A13DA50",
-        To : 'danroxsll@gmail.com',
-        From : "danroxsll@gmail.com",
-        Subject : subject.value,
-        Body : bodyMessage
+        SecureToken: "c13d00cc-7165-4b96-b483-89c1c8683a7a",
+        To: 'danroxsll@gmail.com',
+        From: "danroxsll@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
     }).then(
-      message => {
-        if (message == "OK") {
-            Swal.fire({
-                title: "Success!",
-                text: "Message sent successfully!",
-                icon: "success"
-              });
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Message sent successfully!",
+                    icon: "success"
+                });
+            }
         }
-      }
     );
 }
 
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorTxtEmail.innerText = "Enter a valid email address";
+        }
+        else {
+            errorTxtEmail.innerText = "Email Address can't be blank";
+        }
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    checkInputs();
 
-    sendEmail();
+    if (!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
 });
